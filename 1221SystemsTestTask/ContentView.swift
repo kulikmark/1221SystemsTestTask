4597bc43c0b79236706cb58e29614c97fe2ef5a5
Mark Kulik <kulik_mark@mail.ru>
2024-08-19 15:54:50 +0300

    Initial Commit

---
+++ 1221SystemsTestTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  1221SystemsTestTask
+//
+//  Created by Марк Кулик on 19.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 1221SystemsTestTask/_221SystemsTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  _221SystemsTestTaskApp.swift
+//  1221SystemsTestTask
+//
+//  Created by Марк Кулик on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct _221SystemsTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
